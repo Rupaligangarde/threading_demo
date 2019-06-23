a65a2b8e6237ed7fc3f2f039618c403c4142dc7b
Rupali Gangarde <rupaligangarde11@gmail.com>
2019-06-23 22:09:24 +0530

    threads using lamda expression

--- src/main/java/ThreadDemo.java
+++ src/main/java/ThreadDemo.java
@@ -1,7 +1,16 @@
 public class ThreadDemo{
     public static void main(String[] args) {
-        Worker1 worker1 = new Worker1();
-        Worker2 worker2 = new Worker2();
+
+        Runnable worker1 = ()->{
+            for (int i = 0; i < 10; i++)
+                System.out.println("inside method1");
+        };
+
+        Runnable worker2 = ()->{
+            for (int i = 0; i < 10; i++)
+                System.out.println("inside method2");
+        };
+
         Thread thread1 = new Thread(worker1);
         Thread thread2 = new Thread(worker2);
         thread1.start();
